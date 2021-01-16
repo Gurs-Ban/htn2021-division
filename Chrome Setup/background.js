c5c896619f7cb8ec409e79aa8cf28a8970acc532
Gursimmer <46874155+Gurs-Ban@users.noreply.github.com>
2021-01-16 11:50:36 -0500

    dfgsd

--- Chrome Setup/background.js
+++ Chrome Setup/background.js
@@ -1,5 +1,5 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('index.html', {
+chrome.runtime.onStartup.addListener(function() {
+  chrome.window.create('index.html', {
     id: 'main',
     bounds: { width: 620, height: 500 }
   });
