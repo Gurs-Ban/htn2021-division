2426e7d8ed1fdc92891206b960ae1b97a5640578
Gursimmer <46874155+Gurs-Ban@users.noreply.github.com>
2021-01-16 11:23:26 -0500

    0.001

---
+++ backrground.js
@@ -0,0 +1,17 @@
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('index.html', {
+    id: 'main',
+    bounds: { width: 620, height: 500 }
+  });
+});
+
+// chrome.app.runtime.onLaunched.addListener(function(){
+//     chrome.app.window.create('index.html', {
+//       id:'main',
+//       bounds: {width: 620, height: 500 }
+//     });
+// });
+
+// chrome.alarms.onAlarm.addListener(function( alarm) {
+//   console.log("alarm", alarm);
+// });
